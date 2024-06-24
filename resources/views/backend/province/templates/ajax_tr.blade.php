469aba147dd5181e6402e7e255139bca400c7968
thasocheat <thasocheat167@gmail.com>
2024-06-24 11:36:30 +0700

    Fix and check if no data in database

--- resources/views/backend/province/templates/ajax_tr.blade.php
+++ resources/views/backend/province/templates/ajax_tr.blade.php
@@ -1,4 +1,4 @@
-<tr id="tr_object_id_{{ $row->id }}" class="bgc-h-orange-l4">
+{{-- <tr id="tr_object_id_{{ $row->id }}" class="bgc-h-orange-l4">
     <td>{{ $row->id }}</td>
     <td>{{ $row->name }}</td>
     <td>{{ $row->province_code }}</td>
@@ -9,4 +9,24 @@
     <td>
       @include('backend.templates.crudAction')
     </td>
-</tr>
+</tr> --}}
+
+@if(isset($row))
+    <tr id="tr_object_id_{{ $row->id }}" class="bgc-h-orange-l4">
+        <td>{{ $row->id }}</td>
+        <td>{{ $row->name }}</td>
+        <td>{{ $row->province_code }}</td>
+        <td>{{ $row->created_at ? date('d-M-Y', strtotime($row->created_at)) : '' }}</td>
+        <td>
+            <input id="status" name="status" data-id="{{ $row->id }}" {{ $row->status ? 'checked' : '' }} title="Status" type="checkbox" class="ace-switch input-lg ace-switch-yesno bgc-green-d2 text-grey-m2" />
+        </td>
+        <td>
+            @include('backend.templates.crudAction')
+        </td>
+    </tr>
+@else
+    <tr id="noDataMessage">
+        <td colspan="6">No data available</td>
+    </tr>
+@endif
+
